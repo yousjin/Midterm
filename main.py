029d70661936539778ad02727188934d19605166
yousjin <syg2720@hanmail.net>
2022-11-05 11:14:02 +0900

    Add Log system

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 # Program make a simple calculator
 
+import logging
+
 import Add
 import Sub
 import Mul
 import Div
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+file_handler = logging.FileHandler('my.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 print("Select operation.")
@@ -23,19 +33,32 @@ while True:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
+        if (num1!='1' and num1!='2' and num1!='3' and num1!='4') or (num2!='1' and num2!='2' and num2!='3' and num2!='4'):
+            logger.warning('Invalid INPUT')
+
+
+
         if choice == '1':
             print(num1, "+", num2, "=", Add.add(num1, num2))
+            logger.info('%f + %f = %f', num1, num2, Add.add(num1,num2))
 
         elif choice == '2':
             print(num1, "-", num2, "=", Sub.subtract(num1, num2))
+            logger.info('%f - %f = %f', num1, num2, Sub.subtract(num1,num2))
 
         elif choice == '3':
             print(num1, "*", num2, "=", Mul.multiply(num1, num2))
-            
-        elif choice =='4':
-            print(num1, "/", num2, "=", Div.divide(num1,num2))
-            
+            logger.info('%f * %f = %f', num1, num2, Mul.multiply(num1,num2))
 
+        elif choice =='4':
+            
+            try:
+                print(num1, "/", num2, "=", Div.divide(num1,num2))
+                logger.info('%f / %f = %f', num1, num2, Div.divide(num1,num2))
+            except(ZeroDivisionError):
+                logger.warning('Divide by ZERO')
+                
+            
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
